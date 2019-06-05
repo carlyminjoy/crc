9aae201196214f371fc511ee64606d39463201c6
Carly Minjoy <CarlyMinjoy@cancerqld.org.au>
2019-06-05 15:10:52 +1000

    vue shell for landing page

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,14 @@
         <header>
             <div class='container'>
                 <img src="https://cancerqld.blob.core.windows.net/content/code/global/img/ccq-logo-249x123.png" />
-                <h1>Cancer Risk Quiz</h1>
+                <h1>Cancer Risk Score Card</h1>
             </div>
         </header>
 
         <div class="outer-container">
-            <ul class="progress-bar">
-                <li v-for="(route, index) in routes" :key="index"
-                    :class="{ 'active' : currentStep && index <= routes.indexOf(state.activeCategory) }">
 
-                    <span>{{route}}</span>
-                </li>
-            </ul>
-
-            <div class="results-container" v-if="state.showResults">
-                <h3>Your total score is:</h3>
+            <div class="results-container">
+                <h3>{{ results}} Your total score is:</h3>
 
                 <vue-circle
                     :progress="results.total || 65"
@@ -34,375 +27,37 @@
 
                 </vue-circle>
 
-
-                <template v-if='categoryResults.green.length > 0'>
-
-                    <h3>You're doing great in the following areas:</h3>
-
-                    <ul>
-                        <template v-for="category in scoredCategories">
-                            <li :key="category.name" v-if="results[category.name] >= 66">
-
-                                <i :class="category.icon"></i>
-                                <h4>{{ category.name }}</h4>
-
-                            </li>
-                        </template>
-                    </ul>
-
-                </template>
-
-                <template v-if='categoryResults.amber.length > 0 || categoryResults.red.length > 0'>
-
-                    <h3>The areas you could improve in are:</h3>
-
-                    <ul>
-                        <template v-for="category in scoredCategories">
-                            <li :key="category.name" v-if="results[category.name] < 66">
-
-                                <i :class="category.icon"></i>
-                                <h4>{{ category.name }}</h4>
-
-                            </li>
-                        </template>
-                    </ul>
-                </template>
-
-                <h3>To receive your Cancer Risk Scorecard and tips to reduce your cancer risk, please enter your information below:</h3>
-
-                <form>
-                    <vmd-text-field class='half-width' label='First Name' v-model="form.firstName"></vmd-text-field>
-                    <vmd-text-field class='half-width' label='Last Name' v-model="form.lastName"></vmd-text-field>
-                    <vmd-text-field label='Email Address' v-model="form.emailAddress"></vmd-text-field>
-
-                    <p>By submitting this form, you are agreeing to our <a href='https://cancerqld.org.au/about-us/our-privacy-policy/read-our-privacy-position-statement/' target='_blank'>Privacy Collection Statement</a>.</p>
-
-                    <vmd-button v-if='!state.submitted' :disabled="submitBtnDisabled" :loading="state.loading" text='Get My Cancer Risk Scorecard' @click="sendResults()"></vmd-button>
-                    <p v-else class="confirmation-msg">Thank you! Your form has been submitted.<br>You will receive an email shortly with your recommendations.</p>
-                </form>
-
-            </div>
-
-            <div class="conversation-container mask" ref="conversationEl" v-if="!state.showResults">
-
-                <template class="step-container" v-for="(step, index) in steps">
-
-                    <transition name="fade" :key="index">
-
-                        <div v-if="display(step) && index <= currentStep" class='ai-comment'>
-
-                            <question v-if="step.question && !step.userInput" 
-                                    :question=step 
-                                    v-on:addstep="addStep()"></question>
-
-                            <weight v-else-if="step.userInput && step.category === 'weight'" 
-                                    :question=step 
-                                    :gender="steps[3].score" 
-                                    v-on:addstep="addStep()"></weight>
-
-                            <postcode v-else-if="step.userInput && step.id === 'postcode'" 
-                                    :question=step 
-                                    v-on:addstep="addStep()"></postcode>
-
-                            <p v-else>{{ step.text }}</p>
-
-                        </div>
-
-                    </transition>
-
-                    <transition name="fade" :key="index">
-                        <div v-if="step.score || step.score === 0" class='user-comment'>
-
-                            <p v-if="!step.options"> {{ step.userResponse }} </p>
-
-                            <p v-if="step.options" v-html="step.options.find((o) => o.score === step.score ).userResponse"></p>
-
-                        </div>
-                    </transition>
-
-                    <transition name="fade" :key="index">
-                        <!-- <div v-if="step.userResponded" class='ai-comment'>
-
-                            <p v-if="step.options"> {{ step.aiResponse }} </p>
-
-                            <p v-else v-html="step.options.find((o) => o.score === step.score ).aiResponse"></p>
-
-                        </div> -->
-                    </transition>
-
-                </template>
-
             </div>
         </div>
 	</div>
 </template>
 
-
 <script>
-
-import { default as conversation } from './config.js'
-import JQuery from 'jquery'
-let $ = JQuery
-import { default as Question } from './Question.vue'
-import { default as Weight } from './Weight.vue'
-import { default as Postcode } from './Postcode.vue'
-import { setTimeout } from 'timers';
-import { vmdButton, vmdTextField } from '@ccq/ccq-vue-components'
+import { vmdButton } from '@ccq/ccq-vue-components'
 import VueCircle from 'vue2-circle-progress'
-import axios from 'axios'
-
-
-const timers = {
-    shortest: 500,
-    short: 1000,
-    med: 2000,
-    long: 3000
-}
 
 export default {
     name: 'app',
     components: {
-        Question,
-        Weight,
-        Postcode,
         vmdButton,
-        vmdTextField,
         VueCircle
     },
 	data () {
 		return {
-            state: {
-                showResults: false,
-                loading: false,
-                submitted: false,
-                errors: false,
-                activeCategory: 'personal'
-            },
-            form: {
-                firstName: '',
-                lastName: '',
-                emailAddress: ''
-            },
-            routes: [
-                'personal',
-                'uv', 
-                'smoking',
-                'alcohol',
-                'nutrition',
-                'weight',
-                'physical activity',
-                'screening',
-                'results'
-            ],
-            categoryResults: {
-                red: [],
-                green: [],
-                amber: []
-            },
-            scoredCategories: [
-                {
-                    name: 'uv',
-                    icon: 'fas fa-sun'
-                },
-                {
-                    name: 'smoking',
-                    icon: 'fas fa-smoking'
-                },
-                {
-                    name: 'alcohol',
-                    icon: 'fas fa-wine-bottle'
-                },
-                {
-                    name: 'nutrition',
-                    icon: 'fas fa-utensils'
-                },
-                {
-                    name: 'weight',
-                    icon: 'fas fa-weight'
-                },
-                {
-                    name: 'physical activity',
-                    icon: 'fas fa-running'
-                },
-                {
-                    name: 'screening',
-                    icon: 'fas fa-clinic-medical'
-                }
-            ],
-            currentStep : null,
-            results: '',
-            steps: conversation.steps
+            results: {}
 		}
     },
-    watch: {
-        currentStep (next, prev) {
-            if (this.steps[next].final) {
-                this.state.activeCategory = 'results'
-                this.calculateResults()
-                setTimeout(() => this.state.showResults = true, timers.med)
-                return
-            }
-
-            if (!this.steps[next].question) {
-                setTimeout(() => this.currentStep++, timers.short)
-                return
-            }  else {
-                this.state.activeCategory = this.steps[next].category
-            }
-
-            if (this.steps[next].question && !this.display(this.steps[next])) {
-                setTimeout(() => this.currentStep++, timers.shortest)
-            }
-
-        }
-    },
-    computed: {
-        submitBtnDisabled() {
-            let vm = this;
-
-            return vm.form.firstName === '' 
-                || vm.form.lastName === '' 
-                || vm.form.emailAddress === ''
-        }
-    },
     methods: {
-        getRecommendations() {
+        getScoreCard() {
             let vm = this;
 
-            let answeredQuestions = vm.steps.filter((step) => {
-                return step.question && (step.score !== null) && step.category !== 'personal' && step.id !== 'weight'
-            })
-
-            let recommendations = answeredQuestions.map((q) => {
-                let answer = q.options? q.options.find((o) => o.score === q.score) : null;
-
-                let obj = {
-                    question: q.text,
-                    category: q.category,
-                    answer: answer ? answer.userResponse : q.userResponse,
-                    recommendation: answer ? answer.aiResponse : q.aiResponse
-                }
-
-                if (q.id === 'bmi') { obj.bmi = q.bmi }
-                if (q.id === 'waist') { obj.waist = q.waist }
-
-                return obj;
-            })
-
-            return recommendations;
-        },
-        async getToken() {
-            let vm = this;
-            return new Promise(async (resolve, reject) => {
-            let token = await grecaptcha.execute('6LdVoXkUAAAAAJSPi9AzIFrcumZGtaqoD7vc_nHH', { "action": "webform" });
-                token && token !== undefined ? resolve(token) : reject(false);
-            })
-        },
-        async getPostData() {
-            let vm = this;
-            let gaToken = await vm.getToken();
-
-            let entry = {
-                firstName: vm.form.firstName,
-                lastName: vm.form.lastName,
-                emailAddress: vm.form.emailAddress,
-                postcode: vm.steps[4].score,
-                scores: vm.results,
-                recommendations: vm.getRecommendations()
-            }
-
-            return gaToken ? {
-                'sourceIdentifier': 'cancerriskquiz-webform-c9dc3b17',
-                'entry': entry,
-                'debug': 'development',
-                'gaToken' : gaToken
-            } : null;
-        },
-        async sendResults() {
-            let vm = this;
-            vm.state.loading = true;
-
-            let postData = await vm.getPostData()
-            if (postData) {
-                let response = await axios({
-                    method: 'post',
-                    url: 'https://ccq-form-functions.azurewebsites.net/formsubmit',
-                    timeout: 30000,
-                    data: postData
-                })
-
-                vm.state.loading = false;
-                if (response.data.success) {
-                    vm.state.submitted = true;
-                    return;
-                }
-            }
-
-            vm.state.loading = false;
-            vm.state.errors = true;
-        },
-        calculateResults() {
-            let vm = this;
-            let results = {};
-            let total = 0;
-            let screenTotal = 0;
-
-            vm.steps.forEach((step) => {
-                if (step.question && step.category !== 'personal') {
-
-                    if (step.score !== null) {
-                        if (step.category === 'weight') {
-                            if (step.id) { results.weight = step.score }
-
-                        } else if (results[step.category] !== undefined) {
-                            results[step.category] += Math.round(step.score);
-
-                        } else if (results[step.category] === undefined) {
-                            results[step.category] = Math.round(step.score);
-                        }
-
-                        if (step.category === 'screening') {
-                            screenTotal += 70;
-                        }
-                    }
-                }
-            })
-
-            Object.keys(results).forEach((category) => {
-                let categoryScore = null;
-
-                if (category === 'screening') {
-                    if (screenTotal > 0) {
-                        categoryScore = Math.round(results[category] / screenTotal * 100)
-                    }
-                } else {
-                    categoryScore = Math.round(results[category] / 210 * 100)
-                }
-
-                results[category] = categoryScore
-                vm.categoryResults[categoryScore > 66 ? 'green' : (categoryScore > 33 ? 'amber' : 'red')].push(category)
-                total += categoryScore
-            })
-
-            results.total = Math.round(total / Object.keys(results).length)
-            vm.results = results;
-        },
-        addStep() {
-            let display;
-
-            setTimeout(() => this.steps[this.currentStep].userResponded = true, timers.short)
-            setTimeout(() => this.steps[this.currentStep].aiResponded = true, timers.med)
-            setTimeout(() => {
-                this.display(this.steps[this.currentStep + 1]) ? this.currentStep++ : this.currentStep += 2
-            }, timers.short)
-        },
-        display(step) {
-            let displayExpression = step.display;
-            return eval(displayExpression);
+            
         }
     },
     mounted () {
-        this.currentStep = 0;
+        let vm = this;
+
+        vm.getScoreCard();
     }
 }
 </script>
@@ -463,92 +118,8 @@ body {
 
 }
 
-.progress-bar {
-    display:flex;
-    list-style-type:none;
-    width:660px;
-    max-width: 100%;
-    margin:0 auto;
-    margin-block-start:0px;
-    margin-block-end: 0px;
-    padding-inline-start:0px;
-    font-family: 'Roboto';
-    border-bottom: 4px solid #999;
-
-
-    li {
-        border-bottom: 4px solid #999;
-        padding: 15px 6px 15px 6px;
-        font-size: 12px;
-        text-transform:uppercase;
-        flex-basis: 12.5%;
-        flex-grow:1;
-        margin-bottom: -4px;
-        transition: all 0.3s ease;
-        // text-align:right;
-
-        &.active {
-            border-bottom: 4px solid $yellow;
-            color:$dark-blue;
-            font-weight:bold;
-        }
-    }
-}
-
-
 .outer-container {
-
     background:#eee;
-}
-.conversation-container {
-    &.mask {
-        -webkit-mask-image: -webkit-gradient(linear, left top, left 20%, from(rgba(0,0,0,0)), to(rgba(0,0,0,1)));
-    }
-    overflow-y:hidden;
-    margin: 60px auto;
-    background:rgba(0,0,0,0);
-    padding: 30px;
-    max-width: 600px;
-    max-height: 500px;
-    text-align:left;
-    display:flex;
-    flex-direction:column;
-    justify-content:flex-end;
-
-    p {
-        margin: 8px 0 3px 0;
-    }
-    
-    .ai-comment {
-        background:#fff;
-        padding: 5px 15px;
-        border-radius: 8px 8px 8px 0;
-        max-width: 400px;
-        margin: 6px auto 6px 0;
-        height: auto;
-        @extend %boxshadow;
-
-        a {
-            text-decoration:none;
-            font-weight: bold;
-            color: $blue;
-            transition: 0.3s ease;
-            &:hover {
-                color:$dark-blue;
-            }
-        }
-    }
-
-    .user-comment {
-        background:$blue;
-        color:#fff;
-        padding: 5px 15px;
-        border-radius: 8px 8px 0 8px;
-        max-width:400px;
-        margin: 6px 0 6px auto;
-        height:auto;
-        @extend %boxshadow;
-    }
 }
 
 .results-container {
@@ -582,11 +153,6 @@ body {
 
         p {
             margin: 20px auto;
-            
-            &.confirmation-msg {
-                padding: 20px;
-                background:#eee;
-            }
 
             a {
                 text-decoration:none;
