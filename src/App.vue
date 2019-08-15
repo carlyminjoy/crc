2fec0cf97a8dc1fd899f0f33e3ba514290a98968
Justin Feakes <justinfeakes@cancerqld.org.au>
2019-08-15 11:48:15 +1000

    Adding the blobs and header stuff into the scorecard

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
 	<div id="app">
 
-
+        <BackgroundBlob></BackgroundBlob>
         <div class="outer-container">
+
+             <div class="heading-container">
+                <div class="cancer-council-logo">
+                    <img src="https://www.cancer.org.au/images/cancer_council.png" />
+                </div>
+                <HeaderBlob></HeaderBlob>
+                <h1 class='heading'>Cancer Risk Calculator<span class="yellow-fullstop">.</span></h1>
+            </div>
+
 
             <spinner color='#fcd208' v-if='loading'></spinner>
 
@@ -151,13 +160,18 @@ import VueCircle from 'vue2-circle-progress'
 import axios from 'axios'
 import { default as Tips } from './tips.js'
 import { default as Resources } from './resources.js'
+import { default as HeaderBlob } from './Blob/HeaderBlob.vue'
+import { default as BackgroundBlob } from './Blob/BackgroundBlob.vue'
+
 
 export default {
     name: 'app',
     components: {
         vmdButton,
         VueCircle,
-        spinner
+        spinner,
+        HeaderBlob,
+        BackgroundBlob
     },
 	data () {
 		return {
@@ -293,7 +307,53 @@ body {
 
 .outer-container {
     background:#eee;
-    padding: 60px 0;
+    padding: 40px 0;
+
+    
+.heading-container {
+        display: flex;
+        flex-direction: row;
+        width: 660px;
+        max-width: 100%;
+        margin: 0 auto;
+        position: relative;
+
+        // * { animation: fade-in 1s forwards; }
+
+        .cancer-council-logo {
+            background: white;
+            border-radius: 5px; 
+            box-shadow: 0px 0px 2px #00000016;
+            margin-top: 12px;
+            margin-right: 10px;
+            height: 100%;
+            padding-top: 3px;
+            padding-left: 15px; padding-right: 20px;
+            image-rendering: -moz-crisp-edges; /* Firefox */
+            image-rendering: -o-crisp-edges; /* Opera */
+            image-rendering: -webkit-optimize-contrast; /* Webkit (non-standard naming) */
+            image-rendering: crisp-edges;
+            -ms-interpolation-mode: nearest-neighbor; /* IE (non-standard property) */
+            // display: none;
+
+            img {
+                height: 60px;
+                width: auto
+            }
+        }
+
+        h1.heading {
+            font-weight: 800;
+            max-width: 200px;
+            text-align: left;
+            line-height: 1.8rem;
+
+            .yellow-fullstop {
+                font-size: 40px;
+                color: $yellow;
+            }
+        }
+    }
 
     .spinner {
         margin-top:40vh!important;
@@ -315,6 +375,7 @@ body {
     max-width: 960px;
     margin: 30px auto;
     height: auto;
+    position: relative;
     @extend %boxshadow;
 
     .yellow {
@@ -761,6 +822,31 @@ body {
                         } 
                     }
                 }
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 700px) {
+    .outer-container {
+        .heading-container {
+            max-width: 90%;
+            margin:0 auto;
+            .cancer-council-logo {
+                padding-left: 5px;
+                padding-right: 8px;
+                padding-top: 5px;
+                
+                img {
+                    height: 50px;
+                }
+            }
+                        
+            h1.heading {
+                margin-top: 28px;
+                padding-bottom: 0;
+                font-size: 25px;
+                line-height: 1.2rem;
             }
         }
     }
---
+++ src/Blob/BackgroundBlob.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="background-blob">
+
+    <!-- TOP BLOB -->
+    <svg
+      width="970.854"
+      height="944.41"
+      viewBox="0 0 970.854 944.41"
+      class="top-blob"
+    >
+      <g transform="matrix(1, 0, 0, 1, 0, 0)">
+        <path
+          d="M307.536,17.858C695.2-107.494,757.475,176.265,719.575,322.449S488.61,627.04,307.536,627.04,90.23,475.5,44.75,358.49-80.127,143.211,307.536,17.858Z"
+          transform="matrix(-0.85, -0.53, 0.53, -0.85, 634.4, 924.15)"
+        ></path>
+      </g>
+    </svg>
+
+    <!-- ALL THE BOTTOM BLOBS -->
+    <div class="bottom-blobs-container">
+
+      <!-- LIGHTEST BLOB -->
+      <svg
+        width="2275.444"
+        height="1195.158"
+        viewBox="0 0 2275.444 1195.158"
+        class="blob-lightest"
+        v-bind:style="{top: `${this.scrollOffset.lightest}px`}"
+      >
+        <path
+          d="M-12.7,902.611C399.487,1004.5,541.7,887.682,901.05,862.285s722.5,67.3,942.176,63.488,357.3,47.945,357.3,47.945V2053.031H-74.923V908.961Z"
+          transform="translate(74.923 -857.873)"
+        ></path>
+      </svg>
+
+      <!-- MIDDLE BLOB -->
+      <svg
+        width="2275.444"
+        height="1157.326"
+        viewBox="0 0 2275.444 1157.326"
+        class="blob-middle"
+        v-bind:style="{top: `${this.scrollOffset.middle}px`}"        
+      >
+        <path
+          d="M16.014,895.705c373.315,58.41,521.879,229.832,881.227,204.436s672.982-162.533,947.254-35.555,356.026-90.867,356.026-90.867V2053.031H-74.923V895.705Z"
+          transform="translate(74.923 -895.705)"
+        ></path>
+      </svg>
+
+      <!-- DARKEST BLOB -->
+      <svg
+        width="2275.775"
+        height="1227.632"
+        viewBox="0 0 2275.775 1227.632"
+        class="blob-darkest"
+        v-bind:style="{top: `${this.scrollOffset.darkest}px`}"
+      >
+        <path
+          d="M-12.7,902.611c373.315,58.41,563.781-49.52,923.129-74.916S1616,996,1919,995s281.521-21.281,281.521-21.281V2053.031H-74.923V908.961Z"
+          transform="translate(74.923 -825.399)"
+        ></path>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundBlob",
+  props: [
+      'finished'
+  ],
+  data: function() {
+    return {
+      scrollOffset: {
+        lightest: 0,
+        middle: 0,
+        darkest: 0
+      }
+    };
+  },
+
+  methods: {
+    handleScroll(event) {
+      let scrollAmount = window.scrollY;
+      this.scrollOffset.lightest = scrollAmount/15;
+      this.scrollOffset.middle = scrollAmount/7;
+      this.scrollOffset.darkest = scrollAmount/3;
+    }
+  },
+
+  mounted: function() {
+    window.addEventListener('scroll', this.handleScroll);  
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+@keyframes fade-in-bottom {
+  0% { opacity: 0; transform: translateY(200px) scaleX(3); }
+  100% { opacity: 1; transform: translateY(0px) scaleX(1); }
+}
+
+@keyframes fade-in-top {
+  0% { opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(20deg); }
+  100% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg); }
+}
+
+.background-blob {
+  overflow: hidden;
+  position: absolute;
+  height: 100%; width: 100%;
+
+  svg { position: absolute; }
+  path { fill: #009bdc; }
+
+  svg.top-blob {
+    top: -570px;
+    left: -400px;
+    right: 0;
+    margin: 0 auto;
+    animation: 2s 1s fade-in-top forwards;
+    opacity: 0;
+    path { opacity: 0.07; }
+  }
+
+  div.bottom-blobs-container {
+    position: fixed;
+    bottom: 0;
+    height: 50vh;
+    width: 100%;
+    overflow: hidden;
+
+    svg {
+      left: 0;
+      top: 0;
+      opacity: 0;
+      transform-origin: bottom left;
+      animation: 2s 1s fade-in-bottom forwards;
+    }
+
+    svg.blob-lightest {
+      path {
+        opacity: 0.05;
+      }
+    }
+
+    svg.blob-middle {
+      margin-top: 10vh;
+      animation-delay: 1s;
+      path {
+        opacity: 0.35;
+      }
+    }
+
+    svg.blob-darkest {
+      margin-top: 22vh;
+      margin-left: -100px;
+      animation-delay: 2s;
+    }
+  }
+}
+
+@media screen and (max-width: 800px) {
+  svg.top-blob {
+    top: -700px!important;
+  } 
+}
+</style>
